f7955c856af45f0895d1938286c51e97a33078ac
Joel Moriana <joel.moriana@gmail.com>
2023-06-13 09:34:58 +0200

    Added secrets and lambda functions

---
+++ leaderboard_2.0/lambda/post.py
@@ -0,0 +1,80 @@
+import base64
+import copy
+import json
+import os
+import urllib3
+
+import boto3
+import botocore.exceptions
+
+session = boto3.session.Session()
+client = session.client(
+    service_name="secretsmanager",
+)
+
+
+def get_secret(secret_id):
+    try:
+        response = client.get_secret_value(SecretId=secret_id)
+    except botocore.exceptions.ClientError as e:
+        raise e
+    else:
+        if "SecretString" in response:
+            secret = response["SecretString"]
+        else:
+            secret = base64.b64decode(response["SecretBinary"])
+        return json.loads(secret)
+
+
+def lambda_handler(event, context):
+ 
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(event["aws"]["s3_bucket"])
+    objs = list(bucket.objects.filter(Prefix="{}/containers-status".format(event["submission"]["submission_id"])))
+    objs_name = [os.path.basename(o.key) for o in objs]
+    objs_extension = [os.path.splitext(obj_name)[1] for obj_name in objs_name]
+
+    if ".cancel" in objs_extension:
+        submission_status = "CANCELLED"
+    elif objs_extension.count(".done") == 9:
+        submission_status = "FINISHED"
+    else:
+        submission_status = "FAILED"
+
+    results = s3.Object(event["aws"]["s3_bucket"], "{}/evalai/results.json".format(event["submission"]["submission_id"]))
+    results = results.get()["Body"].read().decode('utf-8')
+
+    stdout = s3.Object(event["aws"]["s3_bucket"], "{}/evalai/stdout.txt".format(event["submission"]["submission_id"]))
+    stdout = stdout.get()["Body"].read().decode('utf-8')
+
+    metadata = s3.Object(event["aws"]["s3_bucket"], "{}/evalai/metadata.json".format(event["submission"]["submission_id"]))
+    metadata = metadata.get()["Body"].read().decode('utf-8')
+
+    all_submission_data = copy.deepcopy(event)
+    all_submission_data["submission"]["submission_status"] = submission_status
+    all_submission_data["submission"]["result"] = results
+    all_submission_data["submission"]["stdout"] = stdout
+    all_submission_data["submission"]["stderr"] = ""
+    all_submission_data["submission"]["environment_log"] = ""
+    all_submission_data["submission"]["metadata"] = metadata
+
+    evalai_secrets = get_secret(secret_id="evalai")
+    
+    manager = urllib3.PoolManager()
+    out = json.loads(manager.request(
+        method="PUT",
+        url="{}{}{}{}".format(evalai_secrets["api_server"], "/api/jobs/challenge/", event["challenge_id"], "/update_submission/"),
+        headers={"Authorization": "Bearer {}".format(evalai_secrets["auth_token"]), "Content-Type": "application/json"},
+        body=json.dumps({
+            "submission": all_submission_data["submission_id"],
+            "challenge_phase": all_submission_data["track_id"],
+            "submission_status": all_submission_data["submission_status"],
+            "result": all_submission_data["result"],
+            "stdout": all_submission_data["stdout"],
+            "stderr": all_submission_data["stderr"],
+            "environment_log": all_submission_data["environment_log"],
+            "metadata": all_submission_data["metadata"],
+        })
+    ).data)
+
+    return all_submission_data
---
+++ leaderboard_2.0/lambda/pre.py
@@ -0,0 +1,75 @@
+import base64
+import json
+import urllib3
+
+import boto3
+import botocore.exceptions
+
+manager = urllib3.PoolManager()
+
+session = boto3.session.Session()
+client = session.client(
+    service_name="secretsmanager",
+)
+
+
+def get_secret(secret_id):
+    try:
+        response = client.get_secret_value(SecretId=secret_id)
+    except botocore.exceptions.ClientError as e:
+        raise e
+    else:
+        if "SecretString" in response:
+            secret = response["SecretString"]
+        else:
+            secret = base64.b64decode(response["SecretBinary"])
+        return json.loads(secret)
+
+
+def lambda_handler(event, context):
+
+    evalai_secrets = get_secret(secret_id="evalai")
+    submission_data = json.loads(manager.request(
+        method="GET",
+        url="{0}{1}{2}".format(evalai_secrets["api_server"], "/api/jobs/submission/", event["submission_pk"]),
+        headers={"Authorization": "Bearer {}".format(evalai_secrets["auth_token"])},
+    ).data)
+    phase_data = json.loads(manager.request(
+        method="GET",
+        url="{0}{1}{2}".format(evalai_secrets["api_server"], "/api/challenges/challenge/phase/", event["phase_pk"]),
+        headers={"Authorization": "Bearer {}".format(evalai_secrets["auth_token"])},
+    ).data)
+
+    if phase_data.get("codename", "").startswith("leaderboard-10"):
+        cluster_secrets = get_secret(secret_id="beta-leaderboard-10")
+    else:
+        cluster_secrets = get_secret(secret_id="beta-leaderboard-20")
+
+    return {
+        "cluster": {
+            "name": phase_data.get("codename", "").rsplit("-", 1)[0],
+            "simulator_image": cluster_secrets["simulator_image"],
+            "leaderboard_image": cluster_secrets["leaderboard_image"],
+            "logcopy_image": cluster_secrets["logcopy_image"]
+        },
+        "submission": {
+            "submission_name": "submission-{}".format(str(event["submission_pk"])),
+            "challenge_id": str(event["challenge_pk"]),
+            "submission_id": str(event["submission_pk"]),
+            "team_id": str(submission_data.get("participant_team", "")),
+            "team_name": str(submission_data.get("participant_team_name", "")),
+            "submission_status": str(submission_data.get("status", "")),
+            "track_id": str(event["phase_pk"]),
+            "track_codename": phase_data.get("codename", "").rsplit("-", 1)[1],
+            "resume": "",
+            "submitted_image_uri": str(event["submitted_image_uri"])
+        },
+        "aws": {
+            "s3_bucket": cluster_secrets["s3_bucket"],
+            "dynamodb_table": cluster_secrets["dynamodb_table"]
+        },
+        "evalai": {
+            "auth_token": evalai_secrets["auth_token"],
+            "api_server": evalai_secrets["api_server"]
+        }
+    }
