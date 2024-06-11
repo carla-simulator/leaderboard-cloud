7f56a5d4857a31220a4fc85b583d3ef8ab691bdb
Joel Moriana <joel.moriana@gmail.com>
2024-06-11 16:38:07 +0200

    new gpu assignment mechanism

--- leaderboard_2.0/docker/leaderboard/gpu_utils/get_gpu_device.sh
+++ leaderboard_2.0/docker/leaderboard/gpu_utils/get_gpu_device.sh
@@ -1,9 +1,9 @@
 
 #!/bin/bash
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+GPU_DEVICE_FILE=${1}
 
-uuid=$(cat ${SCRIPT_DIR}/gpu.txt)
+uuid=$(cat ${GPU_DEVICE_FILE})
 
 readarray -t ALL_GPUS < <(nvidia-smi --query-gpu=index,uuid --format=csv | grep GPU)
 for gpu in "${ALL_GPUS[@]}"; do  if [[ "$gpu" == *"$uuid"* ]]; then DEVICE=$(cut -d , -f 1 <<< $gpu) && break ; fi; done
